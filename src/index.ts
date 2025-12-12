6bad1e1bdd19cdc9d7985dee627202439cc01571
eden benhamou <97126983+eden0501@users.noreply.github.com>
2025-12-12 21:12:17 +0200

    init typescript app (#1)

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (_, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port: ${PORT}`);
+});
