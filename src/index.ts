98e6b392fcf17004d55c632be01e513c38bf1613
eden0501 <eden03.b@gmail.com>
2025-12-13 14:21:10 +0200

    change log

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ const PORT = process.env.PORT || 3000;
 
 initApp().then((app) => {
   app.listen(PORT, () => {
-    console.log(`Example app listening on port: ${PORT}`);
+    console.log(`Petopia Server listening on port: ${PORT}`);
   });
 });
