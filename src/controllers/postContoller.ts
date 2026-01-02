b6a5bf19728fa880a457f98e406af829c5b94f9a
tamikikoz <tamikikoz@gmail.com>
2026-01-02 22:07:50 +0200

    fix(postController): remove function usage

--- src/controllers/postContoller.ts
+++ src/controllers/postContoller.ts
@@ -1,8 +1,7 @@
 import Post from '../models/postModel';
 import BaseController from './baseController';
 import { PostInterface } from '../types/postInterfaces';
-import { mapPostQueryToFilter } from '../utils/postUtils';
 
-const postController = new BaseController<PostInterface>(Post, mapPostQueryToFilter);
+const postController = new BaseController<PostInterface>(Post);
 
 export default postController;
