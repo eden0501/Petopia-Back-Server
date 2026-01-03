c585ca91700a7aab2b39f48175f13ae0e6ba94aa
tamikikoz <tamikikoz@gmail.com>
2026-01-03 12:36:02 +0200

    Add eslint config file (#14)
    * feat(): add eslint file
    
    * feat(eslint): add new line rule
    
    * fix(eslint): change to double quotes
    
    ---------
    
    Co-authored-by: eden0501 <eden03.b@gmail.com>

---
+++ eslint.config.cjs
@@ -0,0 +1,24 @@
+const tsPlugin = require("@typescript-eslint/eslint-plugin");
+const tsParser = require("@typescript-eslint/parser");
+
+module.exports = [
+  {
+    files: ["**/*.js", "**/*.ts"],
+    ignores: ["dist/**", "node_modules/**"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      quotes: ["error", "double"],
+      "eol-last": ["error", "always"],
+    },
+  },
+];
