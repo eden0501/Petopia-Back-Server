7263c5d95ef57dcb7db1755be1a0540de4ed923a
tamikikoz <tamikikoz@gmail.com>
2026-01-03 12:31:18 +0200

    Add models, controllers and routes for posts api (#11)
    * feat(): temporary mongodb connection + posts model definition
    
    * feat(): add conrollers and routes + user model
    
    * feat(): full posts request implementation
    
    * fix(): add interfaces and typings
    
    * fix(): remove test changes
    
    * fix(): remove eslint definition
    
    * fix(): cr changes
    
    * fix(): final cr changes
    
    * fix(): remove tests file
    
    * fix(postUtils): remove query mappings
    
    * fix(postController): remove function usage

---
+++ src/models/userModel.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import { UserInterface } from '../types/userInterfaces';
+
+const userSchema = new mongoose.Schema<UserInterface>({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String, 
+    required: true,
+  }, 
+  dateOfBirth: {
+    type: Date, 
+    default: Date.now,
+  },
+  petsCount: {
+    type: Number, 
+    default: 0, 
+  }
+});
+
+
+export default mongoose.model('User', userSchema);
