2ee89d4c5ceee647815ed9520221a8730fc06b03
tamikikoz <tamikikoz@gmail.com>
2026-04-05 00:27:05 +0300

    fix(): use nginx instead of https

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,12 @@
-import fs from "fs";
-import https from "https";
 import dotenv from "dotenv";
-
 dotenv.config();
 
 import initApp from "./server";
 
-const NODE_ENV = process.env.NODE_ENV || "development";
-const PORT = NODE_ENV === "production" ? process.env.HTTPS_PORT : process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 initApp().then((app) => {
-  let serverInstance;
-
-  serverInstance =
-    NODE_ENV === "production"
-      ? https.createServer(
-          {
-            key: fs.readFileSync(process.env.HTTPS_KEY_PATH || ""),
-            cert: fs.readFileSync(process.env.HTTPS_CERT_PATH || ""),
-          },
-          app,
-        )
-      : app;
-
-  serverInstance.listen(PORT, () => {
+  app.listen(PORT, () => {
     console.log(`Petopia Server listening on port: ${PORT}`);
   });
 });
