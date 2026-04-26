3d03992510f436d2963d57bc2af2eb572f91908b
tamikikoz <tamikikoz@gmail.com>
2026-04-26 23:09:31 +0300

    fix(): remove swagger description

--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -17,7 +17,6 @@ const options: swaggerJsdoc.Options = {
     servers: [
       {
         url: process.env.BASE_URL || "http://localhost:3000",
-        description: "Development server",
       },
     ],
     components: {
